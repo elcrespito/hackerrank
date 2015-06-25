aaf2dc1ebe6dabf9dd9151846e08d22e3a75f364
Viktor <sagitariusvs@gmail.com>
2015-06-25 14:13:16 +0300

    Task Lonely integer

---
+++ Rank1/LonelyInteger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.Rank1
+{
+    public class LonelyInteger
+    {
+        public static int Solution(int[] a)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (dict.ContainsKey(a[i]))
+                {
+                    dict[a[i]]++;
+                }
+                else
+                {
+                    dict[a[i]] = 1;
+                }
+            }
+
+            return dict.Single(p => p.Value == 1).Key;
+        }
+    }
+}
