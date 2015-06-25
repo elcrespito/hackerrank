ec5ffa4712f07188d71a93512eb68b8b90401e81
Viktor <sagitariusvs@gmail.com>
2015-06-25 10:08:13 +0300

    Added Rank 1 tasks
    Diagonal Difference and Pangrams algorighmt

---
+++ Rank1/DiagonalDifference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank
+{
+    public class DiagonalDifference
+    {
+        public static int Answer(int length, string[] matrix)
+        {
+            int leftDiagonal = 0;
+            int rightDiagonal = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                string[] matrixKeys = matrix[i].Split(' ');
+                leftDiagonal += int.Parse(matrixKeys[i]);
+                rightDiagonal += int.Parse(matrixKeys[length - i - 1]);
+            }
+
+            return Math.Abs(leftDiagonal - rightDiagonal);
+        }
+    }
+}
---
+++ Rank1/Pangrams.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank
+{
+    public class Pangrams
+    {
+        static void solution(string text)
+        {
+            text = Console.ReadLine().ToUpper().Replace(" ", "");
+
+            int sum = 0;
+            for (int i = 0; i < LETTERS_COUNT; i++)
+            {
+                sum += i;
+            }
+
+            foreach (char key in text)
+            {
+                if (Alphabet[key] == false)
+                {
+                    Alphabet[key] = true;
+                    sum -= (key - CHAR_DIFFERENCE);
+                }
+            }
+
+            if (sum == 0)
+            {
+                Console.Write("pangram");
+            }
+            else
+            {
+                Console.Write("not pangram");
+            }
+
+            Console.ReadLine();
+        }
+
+
+        private static Dictionary<char, bool> Alphabet
+        {
+            get
+            {
+                if (_alphabet == null)
+                {
+                    _alphabet = new Dictionary<char, bool>();
+
+                    for (int i = 0; i < LETTERS_COUNT; i++)
+                    {
+                        _alphabet.Add(Convert.ToChar(i + CHAR_DIFFERENCE), false);
+                    }
+                }
+
+                return _alphabet;
+            }
+        }
+
+        private static Dictionary<char, bool> _alphabet;
+
+        private const int CHAR_DIFFERENCE = 65;
+        private const int LETTERS_COUNT = 26;
+    }
+}
